d1caffc945cad22c0c54e0fea1eca2b87b903f71
whizma <isak@maatta.se>
2022-02-11 13:20:34 +0100

    Added logger, writes to log.txt

---
+++ Application/src/Logger.java
@@ -0,0 +1,39 @@
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.IOException;
+
+public class Logger {
+
+	private File file;
+
+	public static void main(String[] args) {
+		Logger logger = new Logger("./test_files/log.txt");
+		logger.log("Hejhej");
+		logger.log("Hallå");
+
+	}
+
+	public Logger(String file) {
+	this.file = new File(file);
+	}
+
+	public void log(String logMessage) {
+		LocalDateTime timeNow = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String formatDateTime = timeNow.format(formatter);
+		try {
+			PrintWriter writer = new PrintWriter(new FileWriter(file, true));
+
+			writer.append(formatDateTime + " ; " + logMessage + "\n");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
