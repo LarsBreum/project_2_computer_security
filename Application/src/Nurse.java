9b5b7094d556fa10720fc01e0ed7b799dbf46154
William Rittfeldt <william.rittfeldt@gmail.com>
2022-02-21 15:06:00 +0100

    added classes

--- Application/src/Nurse.java
+++ Application/src/Nurse.java
@@ -1,4 +1,15 @@
+import java.util.List;
 
-public class Nurse {
-
+public class Nurse extends Person{
+	private String division;
+	private List<Patient> list;
+	
+	public Nurse(String division, String name, String ssn, String role) {
+		super(name, ssn, role);
+		this.division = division;
+	}
+	
+	public void addAsso(Patient patient) {
+		list.add(patient);
+	}
 }
--- Application/src/Person.java
+++ Application/src/Person.java
@@ -1,4 +1,27 @@
 
 public class Person {
-
+	String name;
+	String ssn;
+	String role;
+	
+	public Person(String name, String ssn, String role) {
+		this.name = name;
+		this.ssn = ssn;
+		this.role = role;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSsn() {
+		return ssn;
+	}
+	
+	public String toString() {
+		//StringBuilder sb = new StringBuilder();
+		return "Role: "+ role + "Name: " + name + ", ssn: " + ssn;
+	}
+	
 }
