c3d0ca38594e33b836b9f82b867d68b6483d7a3d
whizma <isak@maatta.se>
2022-02-21 14:17:58 +0100

    action authenticator shell

---
+++ Application/src/ActionAuthenticator.java
@@ -0,0 +1,27 @@
+public class ActionAuthenticator {
+	private Logger logger;
+	
+	public Authenticator(String filename) {
+		logger = new Logger(filename);
+	}
+	
+	public canCreate() {
+	
+	}
+	
+	public canList() {
+	
+	}
+	
+	public canRead() {
+	
+	}
+	
+	public canWrite() {
+	
+	}
+	
+	public canDelete() {
+	
+	}
+}
