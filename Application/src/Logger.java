7cd53c6e6a83780b691d30d2295e6265079beab6
whizma <isak@maatta.se>
2022-02-21 13:41:40 +0100

    test

--- Application/src/Logger.java
+++ Application/src/Logger.java
@@ -7,33 +7,32 @@ import java.io.File;
 import java.io.IOException;
 
 public class Logger {
-
+	
 	private File file;
-
+	
 	public static void main(String[] args) {
 		Logger logger = new Logger("./test_files/log.txt");
-		logger.log("Hejhej");
-		logger.log("Hallå");
-
+		logger.log("Tjabba");
+		logger.log("Tjosan");
 	}
-
+	
 	public Logger(String file) {
-	this.file = new File(file);
+		this.file = new File(file);
 	}
-
+	
 	public void log(String logMessage) {
 		LocalDateTime timeNow = LocalDateTime.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		String formatDateTime = timeNow.format(formatter);
 		try {
 			PrintWriter writer = new PrintWriter(new FileWriter(file, true));
-
+			
 			writer.append(formatDateTime + " ; " + logMessage + "\n");
 			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		
 	}
-
+	
 }
