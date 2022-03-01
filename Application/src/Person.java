9fcab4898dc50cb19e26301e97ebcb4d9f02139c
whizma <isak@maatta.se>
2022-03-01 15:12:34 +0100

    lagt till grejer

--- Application/src/Person.java
+++ Application/src/Person.java
@@ -1,16 +1,18 @@
+import java.util.HashSet;
 
 public class Person {
 	String name;
 	String ssn;
 	String role;
 	String division;
+	HashSet<String> patients;
 	
 	public Person(String name, String ssn, String role, String division) {
 		this.name = name;
 		this.ssn = ssn;
 		this.role = role;
 		this.division = division;
-		
+		patients = new HashSet<>();
 	}
 	
 	public String getName() {
@@ -27,6 +29,33 @@ public class Person {
 	
 	public String getDivision() {
 		return division;
+	}
+	
+	public void addPatient(String p) {
+		patients.add(p);
+	}
+	
+	public boolean isRole(String r) {
+		return role.equals(r);
+	}
+	
+	public boolean isTreating(String p) {
+		return patients.contains(p);
+	}
+	
+	public boolean inDivision(String d) {
+		return division.equals(d);
+	}
+	
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		Person p = (Person) o;
+		return name.equals(p.getName());
 	}
 	
 	public String toString() {
